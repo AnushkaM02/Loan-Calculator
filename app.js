a2604b5b2ebda257e7d0a0e165a819b80f693970
AnushkaM02 <anushka.mulik@gmail.com>
2021-11-23 22:14:47 +0530

    calculate and error

---
+++ app.js
@@ -0,0 +1,66 @@
+//Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    //
+    
+    //Show loader
+    
+    e.preventDefault();
+});
+
+//Calculate results
+function calculateResults(e){
+    console.log('Calculating...');
+    //UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parsefloat(amount.value);   //calculates principal
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;    //calculates interest
+    const calculatedPayments = parseFloat(years.value) * 12;        //calculates payments
+
+    //Compute monthly payment
+    const x = Math.pow(1+calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x-1);    
+
+    if (isFinite(monthly)){        //if monthly is finite, display results
+        monthlyPayment.value = monthly.toFixed(2);      //toFixed prints to 2 decimal places
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    }
+    else{
+        showError('Please check your numbers');
+    }
+
+    e.preventDefault();
+}
+
+//Show error
+function showError(error){
+    //Create a div
+    const errorDiv = document.createElement('div');
+
+    //Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    //Add class
+    errorDiv.className = 'alert alert-danger';
+
+    //Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    //Insert error above heading
+    card.insertBefore(errorDiv, heading);    //pass in element to put in, whatever you want to insert it before
+
+    //Clear error after 3 seconds
+    setTimeout(clearError, 3000);    //parameters are function, time in milliseconds
+
+    //Clear error
+    function clearError(){
+        document.querySelector('.alert').remove();
+    }
+}
